6377eaa54ed22532330f65b3344e1be73ebf88ae
JustAnotherCoder101 <132294445+JustAnotherCoder101@users.noreply.github.com>
2023-06-07 17:18:58 +0000

    Add replit and features

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
-Name= input("What is Your Hero's name?")
+import random
+import os
+Name = input("What is Your Hero's name?")
+HP = 100
+MP = 70
+os.system("clear")
+print("Hero Stats")
+print("Name: "+Name)
+print("HP: "+str(HP))
+print("MP: "+str(MP))
