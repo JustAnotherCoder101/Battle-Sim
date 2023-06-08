df692b2746536cb4971afa0c4c67172a4a00675f
JustAnotherCoder101 <132294445+JustAnotherCoder101@users.noreply.github.com>
2023-06-08 12:57:26 +0000

    Formatted code

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import random
 import os
+
 Name = input("What is Your Hero's name?")
 HP = 100
 MP = 50
@@ -10,8 +11,8 @@ XPlvl = 10
 Lvl = 1
 os.system("clear")
 print("Hero Stats")
-print("Name: "+Name)
-print("HP: "+str(HP)+"/"+str(HPM))
-print("MP: "+str(MP)+"/"+str(MPM))
-print("Lvl: "+str(Lvl))
-print("XP: "+str(XP)+"/"+str(XPlvl))
+print("Name: " + Name)
+print("HP: " + str(HP) + "/" + str(HPM))
+print("MP: " + str(MP) + "/" + str(MPM))
+print("Lvl: " + str(Lvl))
+print("XP: " + str(XP) + "/" + str(XPlvl))
