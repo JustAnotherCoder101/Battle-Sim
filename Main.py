e94a7b8ba2bb8d2663c4a423782a019e750a445e
JustAnotherCoder101 <132294445+JustAnotherCoder101@users.noreply.github.com>
2023-06-08 10:59:03 +0000

    Added more stats

--- Main.py
+++ Main.py
@@ -3,8 +3,13 @@ import os
 Name = input("What is Your Hero's name?")
 HP = 100
 MP = 70
+XP = 0
+XPlvl = 10
+Lvl = 1
 os.system("clear")
 print("Hero Stats")
 print("Name: "+Name)
 print("HP: "+str(HP))
 print("MP: "+str(MP))
+print("Lvl:"+str(Lvl))
+print("XP:"+str(XP)+"/"+str(XPlvl))
