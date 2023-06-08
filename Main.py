ad9fa108b116528f6d2b02e9784af2c9621e8fc3
JustAnotherCoder101 <132294445+JustAnotherCoder101@users.noreply.github.com>
2023-06-08 12:59:49 +0000

    Added stats

--- Main.py
+++ Main.py
@@ -1,15 +1,19 @@
 import random
 import os
+
 Name = input("What is Your Hero's name?")
 HP = 100
-MP = 70
+MP = 50
+HPM = 100
+MPM = 50
 XP = 0
 XPlvl = 10
 Lvl = 1
 os.system("clear")
 print("Hero Stats")
-print("Name: "+Name)
-print("HP: "+str(HP))
-print("MP: "+str(MP))
-print("Lvl:"+str(Lvl))
-print("XP:"+str(XP)+"/"+str(XPlvl))
+print("Name: " + Name)
+print("HP: " + str(HP) + "/" + str(HPM))
+print("MP: " + str(MP) + "/" + str(MPM))
+print("Lvl: " + str(Lvl))
+print("XP: " + str(XP) + "/" + str(XPlvl))
+
