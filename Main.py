c0afb145cf73158aadd969a22cccf2f0272b51ac
JustAnotherCoder101 <132294445+JustAnotherCoder101@users.noreply.github.com>
2023-06-18 09:50:10 +0000

    Added a possible buff system

--- Main.py
+++ Main.py
@@ -12,14 +12,21 @@ XPlvl = 10
 Lvl = 1
 BDAM = 3
 Coins = 10
-Slot0 = {"0": 0}
+Slot0 = {"0": 0, "WoodHelmet": 3}
+#Slots are numbered in the id of the list
 Slot1 = {"Shirt": 2}
-Slot2= {"Leggings": 1}
-Slot3 = {"Twig": 5}
-Slot4 = {"0": 0}
-Slot = ["0", "Shirt", "Leggings", "Twig", "0"]
+Slot2 = {"Leggings": 1, "PlatedLeggings": 3}
+Slot3 = {"Twig": 4, "Sword": 6}
+SlotBuff = {"0": 0}
+#SlotBuff is slot 4,5 and 6
+Slot = ["0", "Shirt", "Leggings", "Twig", "0", "0", "0"]
 DAM = Slot3.get(Slot[3]) + BDAM
-DEF = Slot0.get(Slot[0]) + Slot1.get(Slot[1]) + Slot2.get(Slot[2]) + Slot4.get(Slot[4])
+DEF = Slot0.get(Slot[0]) + Slot1.get(Slot[1]) + Slot2.get(Slot[2])
+BUFF = [
+  SlotBuff.get(Slot[4]),
+  SlotBuff.get(Slot[5]),
+  SlotBuff.get(Slot[6]),
+]
 os.system("clear")
 print("Hero Stats")
 print("Name: " + Name)
