ff435e0ef49be1b2394f0429d723af6f2ced403e
JustAnotherCoder101 <132294445+JustAnotherCoder101@users.noreply.github.com>
2023-06-20 15:51:18 +0000

    added dict buff and crit chance

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import random
 import os
 import function
- 
+
 Name = input("What is Your Hero's name?")
 HP = 20
 MP = 20
@@ -11,13 +11,22 @@ XP = 0
 XPlvl = 10
 Lvl = 1
 BDAM = 3
+Crit = 4
 Coins = 10
 Slot0 = {"0": 0, "WoodHelmet": 3}
 #Slots are numbered in the id of the list
 Slot1 = {"Shirt": 2}
 Slot2 = {"Leggings": 1, "PlatedLeggings": 3}
 Slot3 = {"Twig": 4, "Sword": 6}
-SlotBuff = {"0": 0}
+SlotBuff = {
+  "0": 0,
+  "Crit1": 5,
+  "Crit2": 10,
+  "Def1": 5,
+  "Def2": 15,
+  "Dam1": 10,
+  "Dam2": 20
+}
 #SlotBuff is slot 4,5 and 6
 Slot = ["0", "Shirt", "Leggings", "Twig", "0", "0", "0"]
 DAM = Slot3.get(Slot[3]) + BDAM
@@ -27,6 +36,7 @@ BUFF = [
   SlotBuff.get(Slot[5]),
   SlotBuff.get(Slot[6]),
 ]
+
 os.system("clear")
 print("Hero Stats")
 print("Name: " + Name)
@@ -35,8 +45,9 @@ print("MP: " + str(MP) + "/" + str(MPM))
 print("Lvl: " + str(Lvl))
 print("XP: " + str(XP) + "/" + str(XPlvl))
 print("Defense: " + str(DEF))
-print("Damage: " + str(DAM),"\033[33m")
-print("Coins:" + str(Coins),"\033[0m")
+print("Crit chance: " + Crit)
+print("Damage: " + str(DAM), "\033[33m")
+print("Coins:" + str(Coins), "\033[0m")
+
 input("press enter to continue")
 os.system("clear")
-
